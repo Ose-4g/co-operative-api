e13bf9b52f795f2f01a28107f59ac5bff5ecf73a
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-02-15 18:05:43 +0100

    feat: update the port of the database connection

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -2,10 +2,14 @@ import { Dialect, Sequelize } from 'sequelize';
 
 import config from '../config/config';
 const { username, password, database, dialect, host } = config.development;
-const sequelize = new Sequelize(database, username, password, {
+let sequelize = new Sequelize(database, username, password, {
   host: host,
   dialect: dialect as Dialect,
   port: 5432,
 });
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL as string);
+}
 
 export default sequelize;
