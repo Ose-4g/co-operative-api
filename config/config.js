c19b5cfee8c177b053eb7fa464185773aca09cd5
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-02-15 17:40:08 +0100

    feat: update config.js for productio

--- config/config.js
+++ config/config.js
@@ -7,4 +7,12 @@ module.exports = {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
+
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  }
 };
